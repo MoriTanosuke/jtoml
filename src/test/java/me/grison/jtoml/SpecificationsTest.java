836fe46839bb1e64cbfa84162a32a33fdad60597
Carsten Ringe <carsten@kopis.de>
2013-02-25 08:14:38 +0100

    Set java version=1.7.
    Also added static imports to the SpecificationsTest to avoid import of
    Assert.

--- src/test/java/me/grison/jtoml/SpecificationsTest.java
+++ src/test/java/me/grison/jtoml/SpecificationsTest.java
@@ -1,43 +1,47 @@
 package me.grison.jtoml;
 
-import junit.framework.Assert;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Locale;
+
+import org.junit.Test;
 
 public class SpecificationsTest {
     static {
         Locale.setDefault(Locale.US);
     }
+
     @Test
     public void testInteger() {
         Toml toml = Toml.parse("foo = 42");
-        Assert.assertEquals(42, toml.getInt("foo").intValue());
+        assertEquals(42, toml.getInt("foo").intValue());
     }
 
     @Test
     public void testFloat() {
         Toml toml = Toml.parse("foo = 13.37");
-        Assert.assertEquals(13.37, toml.getFloat("foo").floatValue(), 0.00001f);
+        assertEquals(13.37, toml.getFloat("foo").floatValue(), 0.00001f);
     }
 
     @Test
     public void testBoolean() {
         Toml toml = Toml.parse("foo = true\nbar = false");
-        Assert.assertEquals(true, toml.getBoolean("foo").booleanValue());
-        Assert.assertEquals(false, toml.getBoolean("bar").booleanValue());
+        assertEquals(true, toml.getBoolean("foo").booleanValue());
+        assertEquals(false, toml.getBoolean("bar").booleanValue());
     }
 
     @Test
     public void testString() {
         Toml toml = Toml.parse("foo = \"Hello\\tWorld\\nI'm having \\\"!\"");
-        Assert.assertEquals("Hello\tWorld\nI'm having \"!", toml.getString("foo"));
+        assertEquals("Hello\tWorld\nI'm having \"!", toml.getString("foo"));
     }
 
     @Test
     public void testArray() {
-        Toml toml = Toml.parse("foo = [\n\"Hello\",\n\n\t \"World\"\n,\"Nice\"]");
-        Assert.assertEquals(Arrays.asList("Hello", "World", "Nice"), toml.getList("foo"));
+        Toml toml = Toml
+                .parse("foo = [\n\"Hello\",\n\n\t \"World\"\n,\"Nice\"]");
+        assertEquals(Arrays.asList("Hello", "World", "Nice"),
+                toml.getList("foo"));
     }
 }
